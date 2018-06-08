f7ca8620c790ec0099b47bf27908049abe0376c0
Maxim Schutsky <maximuzz@yandex.ru>
2018-06-08 15:27:58 +0300

    Update webpack.config.js
    Added some plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   devtool: 'inline-source-map',
-  entry: ["./src/index.js"],
+  entry: { bundle: "./src/index.js" },
   output: {
-    path: __dirname,
+    path: path.join(__dirname, "dist"),,
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "[name].[chunkhash].js"
   },
   watch: true,
   module: {
     rules: [
       {
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: "babel-loader",
         query: {
           presets: ["react", "es2015", "stage-1"]
-        }
+        },
+        test: /\.js$/
       },
       {
         use: ["style-loader", "css-loader"],
@@ -34,5 +39,16 @@ module.exports = {
       aggregateTimeout: 300,
       poll: 1000
     }
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ["vendor", "manifest"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }), 
+    new webpack.DefinePlugin({ //defive windows scope variables for react ensure additional checking in production
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
